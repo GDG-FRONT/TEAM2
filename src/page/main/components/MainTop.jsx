01a27e91837a5a62416f79bfe64a955483b86899
taetae <dlaxodus1212@naver.com>
2024-11-09 20:52:05 +0900

    Feat: main 완성

--- src/page/main/components/MainTop.jsx
+++ src/page/main/components/MainTop.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import warterImage from "../../../assets/Main/10pro.svg";
-import "../../../scss/main/_mainTop.scss";  // SCSS 모듈을 불러오기
+import "../../../scss/main/_mainTop.scss"; // SCSS 모듈을 불러오기
 
-const MainTop = () => {
-    const [title, setTitle] = useState("물세레로 구원을 받으세요");
-    const [targetWater, setTargetWater] = useState("2L")
-
-    return (
-      <div className="mainTopPage">
-        <div className="main-title">{title}</div>
-        <div className="target-water">오늘의 목표 섭취량: {targetWater}</div>
-        <img src={warterImage} alt="물 이미지" className="main-water-image"></img>
-      </div>  
-    );
+const MainTop = ({ title }) => {
+  return (
+    <div className="mainTopPage">
+      <div className="main-title">물세레로 구원을 받으세요</div>
+      <div className="target-water">오늘의 목표 섭취량: 2L</div>
+      <img src={warterImage} alt="물 이미지" className="main-water-image" />
+    </div>
+  );
 };
 
 export default MainTop;
