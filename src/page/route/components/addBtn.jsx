e449d68c7224fdcff23f689d7c0d5a661542b88d
Nam031001 <127704144+Nam031001@users.noreply.github.com>
2024-11-18 15:17:06 +0900

    Feat:almostEnd

--- src/page/route/components/addBtn.jsx
+++ src/page/route/components/addBtn.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../../../scss/route/Btn.scss';
+import { useNavigate } from 'react-router-dom';
 
-const AddBtn = () => {
+const AddBtn = ({ isStatic }) => {
+  const navigate = useNavigate();
+
+  const warningGo = () => {
+    navigate('/warning');
+  };
   return (
-    <div class="addContainer">
-      <button class="add">추가하기</button>
+    <div className={`addContainer ${isStatic ? 'staticPosition' : ''}`}>
+      <button class="add" onClick={warningGo}>
+        추가하기
+      </button>
     </div>
   );
 };
